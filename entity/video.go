b85e17ac264841aa6c8c2bee8a0301a27193fe1f
Vedant Pareek <dunefro@gmail.com>
2023-06-07 14:51:46 +0530

    Added controller entity and service

---
+++ entity/video.go
@@ -0,0 +1,7 @@
+package entity
+
+type Video struct {
+	Ttile       string `json:"title"`
+	Description string `json:"description"`
+	URL         string `json:"url"`
+}
---
+++ server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/dunefro/gin-server/controller"
+	"github.com/dunefro/gin-server/service"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+	server.GET("/test", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "OK!!",
+		})
+	})
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+	server.Run("localhost:9090")
+}
